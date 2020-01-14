f14e18c85ebb685fc5bc4fa5a75e0594273e5fee
ptuor <tuor_philipp@hotmail.com>
2020-01-14 19:29:45 +0100

    Some Unit Test on Tank class

--- src/test/java/TankWarsGame/Tank/TankTest.java
+++ src/test/java/TankWarsGame/Tank/TankTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class TankTest {
 
     @Test
-    void CreateTank_returnHorizontalPosition() {
+    void CreateTank_returnHorizontalPosition_equalsShouldBeTrue() {
         Tank tank = new Tank(5,0);
 
         int resultHorizontalPosition = tank.getHorizontalPosition();
@@ -14,10 +14,17 @@ class TankTest {
         Assertions.assertEquals(5, resultHorizontalPosition);
     }
 
+    @Test
+    void CreateTank_returnHorizontalPosition_equalsShouldBeFalse() {
+        Tank tank = new Tank(5,0);
 
+        int resultHorizontalPosition = tank.getHorizontalPosition();
+
+        Assertions.assertNotEquals(0, resultHorizontalPosition);
+    }
 
     @Test
-    void CreateTankSetHorizontalPosition_getHorizontalPosition_returnsCorrectHorizontalPosition() {
+    void CreateTankSetHorizontalPosition_getHorizontalPosition_equalsShouldBeTrue() {
         Tank tank = new Tank(5,0);
         tank.setHorizontalPosition(3);
 
@@ -25,4 +32,54 @@ class TankTest {
 
         Assertions.assertEquals(3,resultHorizontalPosition);
     }
+
+    @Test
+    void CreateTankSetHorizontalPosition_getHorizontalPosition_equalsShouldBeFalse() {
+        Tank tank = new Tank(5,0);
+        tank.setHorizontalPosition(3);
+
+        int resultHorizontalPosition = tank.getHorizontalPosition();
+
+        Assertions.assertNotEquals(1,resultHorizontalPosition);
+    }
+
+    @Test
+    void CreateTank_returnVerticalPosition_equalsShouldBeTrue() {
+        Tank tank = new Tank(0,5);
+
+        int resultVerticalPosition = tank.getVerticalPosition();
+
+        Assertions.assertEquals(5, resultVerticalPosition);
+    }
+
+    @Test
+    void CreateTank_returnVerticalPosition_equalsShouldBeFalse() {
+        Tank tank = new Tank(3,5);
+
+        int resultVerticalPosition = tank.getVerticalPosition();
+
+        Assertions.assertNotEquals(3, resultVerticalPosition);
+    }
+
+    @Test
+    void CreateTankSetVerticalPosition_getVerticalPosition_equalsShouldBeTrue() {
+        Tank tank = new Tank(5,0);
+        tank.setVerticalPosition(3);
+
+        int resultVerticalPosition = tank.getVerticalPosition();
+
+        Assertions.assertEquals(3,resultVerticalPosition);
+    }
+
+    @Test
+    void CreateTankSetVerticalPosition_getVerticalPosition_equalsShouldBeFalse() {
+        Tank tank = new Tank(5,0);
+        tank.setHorizontalPosition(3);
+
+        int resultVerticalPosition = tank.getVerticalPosition();
+
+        Assertions.assertNotEquals(5,resultVerticalPosition);
+    }
+
+
 }
