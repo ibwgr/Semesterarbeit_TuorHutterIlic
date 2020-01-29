3ff308e8e4989aa2dae19e9e658cfed32299929f
ptuor <tuor_philipp@hotmail.com>
2020-01-29 07:43:57 +0100

    refactoring and add some comments at GameLogic class

--- src/main/java/TankWarsGame/GameLogic/GameLogic.java
+++ src/main/java/TankWarsGame/GameLogic/GameLogic.java
@@ -8,9 +8,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class GameLogic extends Thread {
+
     private boolean gameRunning;
     public static GameSequencer gameSequencer;
-
     public GameLogic(){
         this.gameRunning = true;
     }
@@ -18,90 +18,94 @@ public class GameLogic extends Thread {
 
     public void run() {
 
-
         while (gameRunning){
 
             switch (gameSequencer){
 
                 case INIT:
+                    // initial state at startup, nothing to do
                     break;
 
 
                 case CHECK_IF_OPPONENT_IS_WAITING_FOR_CONNECTION:
-                    System.out.println("check if opponent is waiting for connection");
+                    // try to connect to opponent
                     try (
                             Socket opponentSocket = new Socket(MainWindow.opponentHostAddress, MainWindow.opponentPort);
                             PrintWriter toServerOpponent = new PrintWriter(opponentSocket.getOutputStream(),true);
                     ) {
-                        toServerOpponent.println("try connection");
-                        System.out.println("connection to opponent established, change to own turn");
+                        // if no error occurred, the connection to the opponent could be established, game starts with own turn
                         MainWindow.setOpponentTurn(false);
                         gameSequencer = GameSequencer.OWN_TURN;
 
                     } catch ( IOException e) {
-                        System.out.println("connection to opponent not established, change to WAIT_UNTIL_OPPONENT_CONNECTS");
+                        // if an error occur, the connection to the opponent could not be established, we have to wait for connection of the opponent and game starts with opponent turn
                         gameSequencer = GameSequencer.WAIT_UNTIL_OPPONENT_CONNECTS;
                     }
                     break;
 
+
                 case WAIT_UNTIL_OPPONENT_CONNECTS:
+                    // wait until connection from opponent has been accepted
                     try (
                             ServerSocket opponentServer = new ServerSocket(MainWindow.ownPort);
-                            Socket socket = opponentServer.accept();                                  // Client Verbindung akzeptieren
+                            Socket socket = opponentServer.accept();
                             BufferedReader fromServerOpponent = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     ) {
+                        // as soon connection has been accepted, start game with opponent turn
                         String text = fromServerOpponent.readLine();
-                        System.out.println("received connection from opponent");
                         gameSequencer = GameSequencer.SET_OPPONENT_TURN;
 
                     } catch ( IOException e) {
                         System.out.println("something went wrong, this shouldn't happen. --> gameSequencer case WAIT_UNTIL_OPPONENT_CONNECTS");
+                        e.printStackTrace();
                     }
                     break;
 
 
                 case OWN_TURN:
-                    // do nothing
+                    // nothing to do, just wait until own turn is done
                     break;
 
+
                 case CHECK_IF_WON_AFTER_OWN_TURN:
+                    // check if game is finished after own turn
                     if (MainWindow.getOwnScore().intValue() == StartScreen.numberOfTanks) {
-                        System.out.println("You win");
+                        // game is finished, stop gameSequencer Thread
                         gameSequencer = GameSequencer.GAME_OWER;
                     }else{
                         gameSequencer = GameSequencer.SET_OPPONENT_TURN;
                     }
-
                     break;
 
 
                 case SET_OPPONENT_TURN:
-                     MainWindow.setOpponentTurn(true);
-                     gameSequencer = GameSequencer.WAIT_OPPONENT_TURN_IS_DONE;
-                     break;
-
-                case WAIT_OPPONENT_TURN_IS_DONE:
-                    if (!MainWindow.getOpponentPlayerTurn()) {
-                        System.out.println("wait opponent turn is done");
-                        gameSequencer = GameSequencer.CHECK_IF_LOST_AFTER_OPPONENT_TURN;
-                    }
-                    // nothing to do, just wait
+                    // set opponent turn on main window
+                    MainWindow.setOpponentTurn(true);
+                    gameSequencer = GameSequencer.WAIT_OPPONENT_TURN_IS_DONE;
                     break;
 
+
+                case WAIT_OPPONENT_TURN_IS_DONE:
+                    // wait until opponent turn is not active in main window
+                    if (!MainWindow.getOpponentPlayerTurn()) {
+                        gameSequencer = GameSequencer.CHECK_IF_LOST_AFTER_OPPONENT_TURN;
+                    }
+                    break;
+
+
                 case CHECK_IF_LOST_AFTER_OPPONENT_TURN:
-                    MainWindow.setOpponentTurn(false);
-                    System.out.println("check if lost after opponent turn");
+                    // check if game is finished after opponent turn
                     if (MainWindow.getOpponentScore().intValue() == StartScreen.numberOfTanks) {
-                        System.out.println("You loose");
+                        // game is finished, stop gameSequencer Thread
                         gameSequencer = GameSequencer.GAME_OWER;
                     }else{
                         gameSequencer = GameSequencer.OWN_TURN;
                     }
-
                     break;
 
 
                 case GAME_OWER:
+                    // game is over, stop while loop which also ends this Thread
                     System.out.println("game is over");
                     gameRunning = false;
                     break;
